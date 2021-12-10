06082cf784ca2b925893d6a8ffe4e35590c66dd9
yjl <yjl@1026037885@qq.com>
2021-12-10 09:15:25 +0800

    javase 12/10 修改

--- 000-javase/src/com/yjl/system/str/Test.java
+++ 000-javase/src/com/yjl/system/str/Test.java
@@ -1,10 +1,7 @@
 package com.yjl.system.str;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Date;
+import java.util.UUID;
 
 /**
  * @author yjl
@@ -13,23 +10,42 @@ import java.util.Date;
  */
 public class Test {
     public static void main(String[] args) throws IOException {
-        String filename="1.txt";
-        File file = new File("E:" + File.separator + 123456 + File.separator+"test"+File.separator+filename);
-        File parentFile = file.getParentFile();  //创建父级目录  有父级目录才会创建文件
-        System.out.println("parentFile:"+parentFile);
-        String parent = file.getParent();
-        System.out.println("parent:"+parent);
-        if (!parentFile.exists()) {
-            parentFile.mkdirs();
-        }
-            if (!file.exists()) {
-                file.createNewFile();
-            }
+        //随机字符
+        String string = UUID.randomUUID().toString();
+        String replace = string.replace("-", "");
 
-        OutputStream outputStream=new FileOutputStream(file);
-            byte[] bytes=new byte[]{95,96,97};
-            outputStream.write(bytes);
-            outputStream.flush();
-            outputStream.close();
+
+        String filename="11312dadadad.txt";
+        int i = filename.lastIndexOf(".");
+        String substring = filename.substring(i);
+        System.out.println("fileType:"+substring);
+
+        String newFileName = replace + substring;
+        System.out.println("newFileName:"+newFileName);
+
+
+        //File file = new File("E:" + File.separator + 123456 + File.separator+"test"+File.separator+filename);
+        //File parentFile = file.getParentFile();  //创建父级目录  有父级目录才会创建文件
+        //System.out.println("parentFile:"+parentFile);
+        //String parent = file.getParent();
+        //System.out.println("parent:"+parent);
+        //if (!parentFile.exists()) {
+        //    parentFile.mkdirs();
+        //}
+        //    if (!file.exists()) {
+        //        file.createNewFile();
+        //    }
+        //
+        //String path = file.getPath();
+        //System.out.println("path:"+path);
+        //
+        //String absolutePath = file.getAbsolutePath();
+        //System.out.println("absolutePath:"+absolutePath);
+        //
+        //OutputStream outputStream=new FileOutputStream(file);
+        //    byte[] bytes=new byte[]{95,96,97};
+        //    outputStream.write(bytes);
+        //    outputStream.flush();
+        //    outputStream.close();
     }
 }
